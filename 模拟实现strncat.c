ae9832b5254042a9d1f2c1caaeb6368b09987e8e
覃凯鹏 <qinkaipeng0728@gmail.com>
2023-11-28 23:54:00 +0800

    创建 模拟实现strncat.c

---
+++ 模拟实现strncat.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int*my_strncat(char*p,const char*p1,int cout)
+{
+	for (int i = 0; i < cout; i++)
+	{
+		if (*p == *p1)
+		{
+			p++;
+			p1++;
+		}
+		else if (*p < *p1)
+		{
+			return -1;
+		}
+		else
+		{
+			return 1;
+		}
+		return 0;
+	}
+}
+int main()
+{
+	char arr1[20] = "abcdef";
+	char arr2[] = "abcdef";
+	int ret=my_strncat(arr1, arr2, 5);
+	printf("%d", ret);
+	return 0;
+}
